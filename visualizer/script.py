4d4fff57c49064a6f39f2f7241c404400864b1b8
Ben Myers <ben.myers@okstate.edu>
2018-03-26 01:31:14 -0500

    Real-time updates

--- visualizer/script.py
+++ visualizer/script.py
@@ -1,5 +1,4 @@
 from genetic import Population
-from time import sleep
 import sys
 
 target_word, mutation_rate, pop_size = sys.argv[1], float(sys.argv[2]), int(sys.argv[3])
@@ -8,5 +7,4 @@ p = Population(pop_size, mutation_rate, target_word)
 while p.get_best().fitness < 1.0:
     print(''.join(p.get_best().genotype) + '|' + str(p.get_best().fitness))
     p.natural_selection()
-    sleep(0.1)
 print(''.join(p.get_best().genotype) + '|' + str(p.get_best().fitness))
