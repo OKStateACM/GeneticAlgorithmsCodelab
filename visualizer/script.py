684a1429008ddc01323c937ec0c65499a9aa5a71
Andrew Bevelhymer <andrewbeav@protonmail.com>
2018-03-25 23:08:37 -0500

    Fix print statements in script

--- visualizer/script.py
+++ visualizer/script.py
@@ -5,5 +5,5 @@ target_word, mutation_rate, pop_size = sys.argv[1], sys.argv[2], sys.argv[3]
 
 p = Population(pop_size, mutation_rate, target_word)
 while p.get_best().fitness < 1.0:
-    print(''.join(p.get_best().genotype) + '|' + p.get_best().fitness())
-print(''.join(p.get_best().genotype) + '|' + p.get_best().fitness())
+    print(''.join(p.get_best().genotype) + '|' + str(p.get_best().fitness()))
+print(''.join(p.get_best().genotype) + '|' + str(p.get_best().fitness()))
